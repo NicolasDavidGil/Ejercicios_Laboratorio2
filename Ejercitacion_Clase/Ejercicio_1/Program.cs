49a221a42513bbde955c4320139513e9e59a0187
NicolasDavidGil <gp.nicolasdavid@gmail.com>
2022-08-23 11:55:14 -0300

    Ejercicios 1 - 4

---
+++ Ejercitacion_Clase/Ejercicio_1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string nombre;
+
+            nombre = "Nicolas";
+
+            string edadIngresada = Console.ReadLine();
+
+            int edad = int.Parse(edadIngresada);
+
+
+
+            bool resultado = int.TryParse(edadIngresada, out edad);
+
+            if(resultado)
+            {
+                Console.WriteLine("Hola {0} {2}su edad es: {1}", nombre, edad, Environment.NewLine);
+            }
+            else
+            {
+                Console.WriteLine("error en el dato");
+            }
+                */
+            //Ingresar 5 números por consola, guardándolos en una variable escalar. Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+
+
+            string? numeroIngresado;
+            int numeroIterando;
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+            float acumuladorIngresos = 0;
+            int contadorIngresos = 0;
+            bool checkNumero;
+
+            do
+            {
+                Console.WriteLine("Ingrese un numero entero: ");
+                numeroIngresado = Console.ReadLine();
+                checkNumero = int.TryParse(numeroIngresado, out numeroIterando);
+                if (checkNumero)
+                {
+                    acumuladorIngresos += (float)numeroIterando;
+
+                    if (numeroIterando > maxNum)
+                    {
+                        maxNum = numeroIterando;
+                    }
+                    if (numeroIterando < minNum)
+                    {
+                        minNum = numeroIterando;
+                    }
+
+                    contadorIngresos++;
+                }
+                else
+                {
+                    Console.WriteLine("Ingreso incorrecto, solo numeros enteros.");
+                }
+
+            } while (contadorIngresos < 5);
+
+            acumuladorIngresos = acumuladorIngresos / contadorIngresos;
+
+            Console.WriteLine("El numero maximo es: " + maxNum + " , el minimo es: " + minNum + " y  el promedio es: " + acumuladorIngresos);
+        }
+    }
+}
---
+++ Ejercitacion_Clase/Ejercicio_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Un número perfecto es un entero positivo, que es igual a la suma de todos los enteros positivos (excluido el mismo) que son divisores del número.
+
+                El primer número perfecto es 6, ya que los divisores de 6 son 1, 2 y 3; y 1 + 2 + 3 = 6.
+
+                Escribir una aplicación que encuentre los 4 primeros números perfectos.
+             */
+
+            Console.WriteLine("Bienvenido");
+            Console.WriteLine("Vamos a buscar los primeros 4 numeros perfectos.");
+            Console.WriteLine("Un número perfecto es un entero positivo, que es igual a la suma de todos los enteros positivos, excluido el mismo, que son divisores del número.");
+            Console.WriteLine("Empecemos...");
+
+
+            int numeroParaAnalizar = 6; //Se de antemano que es el primer numero perfecto
+            int sumaDeDivisores = 0;
+            int contador = 0;
+
+            do
+            {
+                for (int j = 1; j < numeroParaAnalizar; j++)
+                {
+                    if (numeroParaAnalizar % j == 0)
+                    {
+                        sumaDeDivisores += j;
+                    }
+                }
+                if (sumaDeDivisores == numeroParaAnalizar)
+                {
+                    Console.WriteLine($"Numero perfecto {contador + 1}: {numeroParaAnalizar}");
+                    contador++;
+                }
+                sumaDeDivisores = 0;
+                numeroParaAnalizar++;
+            } while (contador < 4);
+        }
+    }
+}
