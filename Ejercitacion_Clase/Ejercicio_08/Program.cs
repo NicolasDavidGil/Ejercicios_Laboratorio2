2b7562a9ab8c57d4858474648e3ceed47b26511f
NicolasDavidGil <gp.nicolasdavid@gmail.com>
2022-08-25 08:18:08 -0300

    Ejer 8 y 9

---
+++ Ejercitacion_Clase/Ejercicio_08/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Ejercicio_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Escribir un programa que imprima por consola un triángulo como el siguiente:
+
+                *
+                ***
+                *****
+                *******
+                *********
+                El usuario indicará cuál será la altura del triángulo ingresando un número entero positivo. Para el ejemplo anterior, la altura ingresada fue de 5
+             */
+
+            string ingresoUsuario;
+            int numeroIngresado;
+            string dibujando = "*";
+            int contador = 1;
+
+            Console.WriteLine("Ingrese la cantidad de filas que desea dibujar: ");
+            ingresoUsuario = Console.ReadLine();
+            while(!int.TryParse(ingresoUsuario, out numeroIngresado))
+            {
+                Console.WriteLine("Error, solo numeros enteros: ");
+                ingresoUsuario = Console.ReadLine();
+            }
+
+            for (int i = 0; i < numeroIngresado; i++)
+            {
+                Console.WriteLine(Environment.NewLine);
+                for (int j = 1; j <= contador; j++)
+                 {
+                    Console.Write(dibujando);                                  
+                 }
+                
+                contador += 2;
+            }       
+        }
+
+    }
+}
---
+++ Ejercitacion_Clase/Ejercicio_09/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ejercicio_09
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            /*
+             *  Escribir un programa que imprima por consola un triángulo como el siguiente:
+
+                        *
+                       ***
+                      *****
+                     *******
+                    *********
+
+                El usuario indicará cuál será la altura del triángulo ingresando un número entero positivo. Para el ejemplo anterior, la altura ingresada fue de 5.
+             */
+
+            string ingresoUsuario;
+            int numeroIngresado;
+            string dibujo = "*";
+            string espaciado = " ";
+            int cantidadEspaciado;
+            int cantidadDibujos = 1;
+
+
+            Console.WriteLine("Ingrese la altura que desea que tenga el triangulo(max 8): ");
+            ingresoUsuario = Console.ReadLine();
+            while(!int.TryParse(ingresoUsuario, out numeroIngresado))
+            {
+                Console.WriteLine("Error, ingrese solo numeros enteros: ");
+                ingresoUsuario = Console.ReadLine();
+            }
+
+            cantidadEspaciado = numeroIngresado - 1;
+
+            for (int j = 0; j < numeroIngresado; j++)
+            {
+                for (int i = 1; i <= cantidadEspaciado; i++)
+                {
+                    Console.Write(espaciado);                    
+                }
+                for(int k = 0; k < cantidadDibujos; k++)
+                {
+                    Console.Write(dibujo);
+                }
+                Console.WriteLine(Environment.NewLine);
+                cantidadEspaciado -= 1;
+                cantidadDibujos += 2;
+            }
+        }
+    }
+}
